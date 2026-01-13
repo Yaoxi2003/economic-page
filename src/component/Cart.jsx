bcbd9a6316d78a9eb52050762a6664ac3b6a9be9
Yaoxi2003 <Y3517199512@gmail.com>
2026-01-13 12:40:27 +1100

    Update Cart.jsx

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,5 +1,5 @@
 export default function Cart({checkout, number, handleCart, openCart, totalAmount, handleRemove}) {
-    const emptyMessage = (<p className="font-bold text-gray-500">Your cart is empty.</p>)
+    const emptyMessage = (<p className="font-bold">Your cart is empty</p>)
 
     const cartBtn = (
         <button 
@@ -9,24 +9,24 @@ export default function Cart({checkout, number, handleCart, openCart, totalAmoun
             aria-expanded={openCart}
         >
             <img src="/images/icon-cart.svg" alt="" aria-hidden="true" />
-            {checkout && <span className="hint" aria-hidden="true">{number}</span>}
+            {checkout && <p className="hint" aria-hidden="true">{number}</p>}
         </button>
     )
 
     const finalPayment = (
-        <article className="w-full">
-            <div className="flex justify-between items-center gap-4 mb-6">
-                <img src="/images/image-product-1-thumbnail.jpg" alt="Fall Limited Edition Sneakers Thumbnail" className="w-[50px] h-[50px] rounded-md" />
-                <div className="text-gray-500">
-                    <p className="text-sm">Fall Limited Edition Sneakers</p>
-                    <p className="text-sm">{`$125.00 x ${number}`} <span className="font-bold text-grey950">{`$${totalAmount}`}</span></p>
+        <article>
+            <div className="flex justify-between items-center gap-5">
+                <img src="/images/image-product-1-thumbnail.jpg" alt="Fall Limited Edition Sneakers" className="w-[50px] h-[50px] rounded-md" />
+                <div>
+                    <p>Fall Limited Edition Sneakers</p>
+                    <p>{`$125.00 x ${number}`}   <span className="font-bold text-grey950">{`$${totalAmount}`}</span></p>
                 </div>
-                <button onClick={handleRemove} aria-label="Remove item from cart">
+                <button onClick={handleRemove} aria-label="Remove item">
                     <img src="/images/icon-delete.svg" alt="" aria-hidden="true" />
                 </button>
 
             </div>
-            <button className="primary-btn w-full">Checkout</button>
+            <button className="primary-btn sm:w-full mt-6">Checkout</button>
         </article>
     )
 
@@ -36,18 +36,17 @@ export default function Cart({checkout, number, handleCart, openCart, totalAmoun
 
            {openCart && 
             <div 
-                className="absolute z-10 top-20 left-1/2 -translate-x-1/2 sm:translate-0 sm:top-24 lg:left-[unset] lg:right-0 lg:translate-x-0 w-[360px] min-h-[256px] rounded-xl bg-white shadow-2xl"
+                className="absolute z-10 top-20 left-1/2 -translate-x-1/2 sm:translate-0 sm:top-35 lg:left-2/3 lg:-translate-x-1/10 w-[360px] h-[256px] rounded-xl bg-white shadow-lg"
                 role="dialog"
-                aria-label="Shopping Cart Summary"
+                aria-label="Shopping cart"
             >
-                <p className="py-5 px-6 border-b border-grey100 text-grey950 font-bold">Cart</p>
+                <p className="py-5 px-3 border-b border-grey100 text-grey950 font-bold">Cart</p>
 
-                <div className="w-full min-h-[180px] flex justify-center items-center px-6">
-                    {checkout && totalAmount > 0 ? finalPayment : emptyMessage}
+                <div className="w-full h-2/3 flex justify-center items-center">
+                    {checkout && totalAmount!==0 ? finalPayment : emptyMessage}
                 </div>
             </div>
            }
         </>
     )
 }
-
